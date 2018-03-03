59ddbb1beab542884442395182e01c7d422e8e1b
Alex Bakoushin <alex@bakoush.in>
2018-03-03 15:24:25 +0300

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,6 @@
 const commonConfig = require('./build-utils/webpack.common');
 const webpackMerge = require('webpack-merge');
 
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
-
 module.exports =  (env) => {
   console.log(env);
 
